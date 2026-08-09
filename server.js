3fc7a2a0a404661939d0f6820adc2ab0db5ca634
Nikhil Maurya <nikhil@Nikhils-MacBook-Air.local>
2026-08-09 19:29:01 +0530

    Server moved from free plan to paid plan

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.HTTPS==="false",
+      secure: process.env.HTTPS==="true",
       maxAge: 24 * 60 * 60 * 1000,
     },
   })
